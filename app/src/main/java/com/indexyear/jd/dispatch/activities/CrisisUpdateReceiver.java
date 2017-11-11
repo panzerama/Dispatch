3b85b138c42300e4d3fe932da52007997d6ace03
JD Panzer <jason.drew.panzer@gmail.com>
2017-11-10 16:33:50 -0800

    service works, but is cumbersome. redesigning.

--- app/src/main/java/com/indexyear/jd/dispatch/activities/CrisisUpdateReceiver.java
+++ app/src/main/java/com/indexyear/jd/dispatch/activities/CrisisUpdateReceiver.java
@@ -3,8 +3,6 @@ package com.indexyear.jd.dispatch.activities;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
 
 public class CrisisUpdateReceiver extends BroadcastReceiver {
 
@@ -12,8 +10,7 @@ public class CrisisUpdateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+        String crisisString = intent.getStringExtra("example crisis info");
 
-        Log.d(TAG, "Alert Message should fire here!");
     }
 }
