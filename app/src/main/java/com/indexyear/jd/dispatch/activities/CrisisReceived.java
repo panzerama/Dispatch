b01909a424d4e549a4c4eb709bc60af945542a9f
JD Panzer <jason.drew.panzer@gmail.com>
2017-11-11 16:55:50 -0800

    crisis notification data displayed in crisisreceived activity

--- app/src/main/java/com/indexyear/jd/dispatch/activities/CrisisReceived.java
+++ app/src/main/java/com/indexyear/jd/dispatch/activities/CrisisReceived.java
@@ -5,12 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.indexyear.jd.dispatch.R;
 
-public class CrisisReceived extends AppCompatActivity {
+public class CrisisReceived extends AppCompatActivity implements View.OnClickListener{
+
+    final String TAG = "CrisisReceived";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,9 @@ public class CrisisReceived extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextView debugTextView = (TextView) findViewById(R.id.crisisMessageTextView);
+        TextView crisisAddressTextView = (TextView) findViewById(R.id.crisisAddress);
+        TextView crisisTimeTextView = (TextView) findViewById(R.id.crisisTimestamp);
+        Button acceptCrisisButton = (Button) findViewById(R.id.acceptDispatchButton);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -32,8 +38,19 @@ public class CrisisReceived extends AppCompatActivity {
         });
 
         String crisis_address = getIntent().getStringExtra("crisis_address");
-        debugTextView.setText(crisis_address);
+        String crisis_timestamp = getIntent().getStringExtra("crisis_timestamp");
 
+        Log.d(TAG, " receved: " + crisis_address + " " + crisis_timestamp);
+        crisisAddressTextView.setText(crisis_address);
+        crisisAddressTextView.setText(crisis_address);
+
+        acceptCrisisButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "crisis accept button");
     }
 
 }
